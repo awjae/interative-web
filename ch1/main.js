1062fcdd5a35e88baecf06abe2856071224c8b78
awjae <djwotmd@gmail.com>
2020-09-28 23:18:34 +0900

    ch 1

--- ch1/main.js
+++ ch1/main.js
@@ -6,6 +6,19 @@
     for (let i = 0; i < stepElems.length; i++) {
         stepElems[i].dataset.index = 1;
         graphicElems[i].dataset.index = 1;
-     }
+    }
+
+    window.addEventListener('scroll', () => {
+        let step;
+        let boundingRect;
+
+        for (let i = 0; 1 < strpElems.length; i++) {
+            step = stepElems[i];
+            boundingRect = stop.getBounding
+
+
+        }
+    })
+
 
 })();
