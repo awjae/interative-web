14225ab190772df3c9583529418b655f29713fcd
awjae <djwotmd@gmail.com>
2021-02-14 20:35:53 +0900

    grid issue

--- study/zeplin_1/gallery.js
+++ study/zeplin_1/gallery.js
@@ -13,6 +13,19 @@ const THUBNAIL_LIST = [
     { title : 'leaf', path: './image/category_10.jpg' }
 ];
 
+const IMAGE_LIST = [
+    { title : '', category : 'game', path : './image/image_1.jpg', seq : '0001'},
+    { title : '', category : 'game', path : './image/image_2.jpg', seq : '0002' },
+    { title : '', category : 'game', path : './image/image_3.jpg', seq : '0003' },
+    { title : '', category : 'game', path : './image/image_4.jpg', seq : '0004' },
+    { title : '', category : 'game', path : './image/image_5.jpg', seq : '0005' },
+    { title : '', category : 'game', path : './image/image_6.jpg', seq : '0006' },
+    { title : '', category : 'game', path : './image/image_7.jpg', seq : '0007' },
+    { title : '', category : 'game', path : './image/image_8.jpg', seq : '0008' },
+    { title : '', category : 'game', path : './image/image_9.jpg', seq : '0009' },
+    { title : '', category : 'game', path : './image/image_10.jpg', seq : '0010' }
+];
+
 
 gallery.init = (rootEl) => {
     gallery.setHeader(rootEl);
@@ -45,7 +58,28 @@ gallery.setImages = (rootEl) => {
     const categoryEl = document.createElement('section');
     categoryEl.className = 'grid-stack images-wrapper';
     rootEl.insertAdjacentElement('beforeend', categoryEl);
-    gallery.grid = GridStack.init();
+    gallery.grid = GridStack.init({column: 12});
+    for (let i = 0; i < 10; i++) {
+        const contents = `<figure><a href='#'><img src='${IMAGE_LIST[i].path}' alt='${IMAGE_LIST[i].title}'/></a></figure>`;
+        const img = new Image();
+        img.src = IMAGE_LIST[i].path;
+        const ratio = img.width/img.height;
+        if (ratio >= 1.5) {
+            gallery.grid.addWidget({w: 6, h:3, content: contents, noMove: false, noResize: true, locked: false });
+        } else if (ratio <= 0.5) {
+            gallery.grid.addWidget({w: 3, h:6, content: contents, noMove: false, noResize: true, locked: false });
+        } else {
+            gallery.grid.addWidget({w: 4, h:4, content: contents, noMove: false, noResize: true, locked: false });
+        }
+
+        // const contents = document.createElement('figure');
+        // const image = document.createElement('img');
+        // image.src = IMAGE_LIST[i].path;
+        // image.alt = IMAGE_LIST[i].title;
+        // contents.insertAdjacentElement('afterbegin', image);
+        // categoryEl.insertAdjacentElement('afterbegin', contents);
+    }
+
 
 }
 
