26941f77b7aa4d49a2c71c0da1749879b5325f45
awjae <djwotmd@gmail.com>
2021-02-14 16:05:27 +0900

    study part1 set thumbnail

--- study/zeplin_1/gallery.js
+++ study/zeplin_1/gallery.js
@@ -1,10 +1,22 @@
 const gallery = {};
 
-const THUBNAIL_LIST = [];
+const THUBNAIL_LIST = [
+    { title : 'sea', path: './image/category_1.jpg' },
+    { title : 'ani', path: './image/category_2.jpg' },
+    { title : 'flower', path: './image/category_3.jpg' },
+    { title : 'game', path: './image/category_4.jpg' },
+    { title : 'cat', path: './image/category_5.jpg' },
+    { title : 'squirrel', path: './image/category_6.jpg' },
+    { title : 'mark1', path: './image/category_7.jpg' },
+    { title : 'bird', path: './image/category_8.jpg' },
+    { title : 'mark2', path: './image/category_9.jpg' },
+    { title : 'leaf', path: './image/category_10.jpg' }
+];
 
 
 gallery.init = (rootEl) => {
     gallery.setHeader(rootEl);
+    gallery.setThumbnail(rootEl);
 }
 
 gallery.setHeader = (rootEl) => {
@@ -16,11 +28,14 @@ gallery.setHeader = (rootEl) => {
 
 gallery.setThumbnail = (rootEl) => {
 
-    const thumbnailHTML = document.createElement('section');
-    
-
-
-    rootEl.insertAdjacentHTML('afterbegin', headerHTML);
+    const categoryEl = document.createElement('section');
+    categoryEl.className = 'category';
+    THUBNAIL_LIST.forEach(el => {
+        const wrapper = document.createElement('figure');
+        wrapper.insertAdjacentHTML('afterbegin', `<img src='${el.path}' alt='${el.title}' /><figcaption><a href='#'>${el.title}</a></figcaption>`);
+        categoryEl.insertAdjacentElement('beforeend', wrapper);
+    })
+    rootEl.insertAdjacentElement('beforeend', categoryEl);
 
 }
 
