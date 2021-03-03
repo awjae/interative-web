0d15ac78046facf17540b01410013719d92150ab
awjae <djwotmd@gmail.com>
2021-03-03 18:44:13 +0900

    set prod mode webpack

--- study/webpack_2/webpack.config.js
+++ study/webpack_2/webpack.config.js
@@ -1,5 +1,4 @@
 const environment = (process.env.NODE_ENV || 'development').trim();
-
 if (environment === 'development') {
     module.exports = require('./webpack-config/webpack.config.dev');
 } else {
